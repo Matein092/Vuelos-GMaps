a80ad52c48448de248ac340c5aaffad970691149
Matein092 <47835701+Matein092@users.noreply.github.com>
2020-02-02 12:59:54 -0500

    actualizacion

--- model/Flight.cs
+++ model/Flight.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,20 +10,106 @@ namespace model
 {
     class Flight
     {
+        //constantes
+        public const string CSV = "..\\..\\Vuelos.csv";
+
         //Asociaciones
-        private Hashtable cities;
+        public List<City> cities;
+        public City city;
 
 
 
         public Flight()
         {
-            cities = new Hashtable();
+            cities = new List<City>();
         }
 
-        public void readFlights()
+        private void readFlights()
         {
+            StreamReader leer = new StreamReader(CSV);
+            String linea;
+            try
+            {
+                linea = leer.ReadLine();
+                while (linea != null)
+                {
+                    richTextBox1.AppendText(linea + "\n"); 
+                    linea = leer.ReadLine();
+                }
 
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine("EXCEPTION: " + exception.Message);
+            }
         }
+
+        public void citySelected(int index)
+        {
+            city = cities[index];
+        }
+        public string getSelectedOrigin()
+        {
+            string origin = "-";
+            if (city != null)
+            {
+                origin = city.getOrigin();
+            }
+            return origin;
+        }
+
+        public string getSelectedDestination()
+        {
+            string destination = "-";
+            if (city != null)
+            {
+                destination = city.getOrigin();
+            }
+            return destination;
+        }
+        
+        public double GetSelectedLongitude()
+        {
+            double longitude = 0;
+            if (city != null)
+            {
+                longitude = city.getLongitude();
+            }
+            return longitude;
+        }
+
+       
+        public double GetSelectedLatitude()
+        {
+            double latitude = 0;
+            if (city != null)
+            {
+                latitude = city.getLatitude();
+            }
+            return latitude;
+        }
+
+        public string GetSelectedDate()
+        {
+            string date = "-";
+            if (city != null)
+            {
+                date = city.getDate();
+            }
+            return date;
+        }
+
+        public string GetSelectedHour()
+        {
+            string hour = "-";
+            if (city != null)
+            {
+                hour = city.getHour();
+            }
+            return hour;
+        }
+
+
     }
 }
 
